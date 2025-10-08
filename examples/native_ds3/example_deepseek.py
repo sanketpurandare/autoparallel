4ccb3604a09fdd4fadd7ec2a0b57ef809fd4c7bd
Davide Italiano <davidino@meta.com>
2025-10-08 11:23:10 -0700

    [autoparallel] Add missing copyright headers. (#194)

--- examples/native_ds3/example_deepseek.py
+++ examples/native_ds3/example_deepseek.py
@@ -1,7 +1,6 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
 #
-# This source code is licensed under the BSD-style license found in the
+# This source code is licensed under the BSD license found in the
 # LICENSE file in the root directory of this source tree.
 
 from dataclasses import dataclass
--- mast/mount.sh
+++ mast/mount.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
+#
+# This source code is licensed under the BSD license found in the
+# LICENSE file in the root directory of this source tree.
+
 export PS4=' + [$(date +"%Y-%m-%d %H:%M:%S,%3N")] '
 set -eExu -o pipefail
 
